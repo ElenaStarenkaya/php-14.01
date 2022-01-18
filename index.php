5afa0abdd92ebdb15875883bd91fc82e0942f9cc
ElenaStarenkaya <86963339+ElenaStarenkaya@users.noreply.github.com>
2022-01-18 18:40:34 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$mod = 5%2;
+$x = 3;
+$inc = ++$x;
+$inc2 = $x++;
+
+echo "это модуль - $mod, это инкремент 1способ - $inc, $inc2"; 
+echo $inc, "<br />";
+echo($inc2);
+
+$x=4;
+$y='4';
+$x==$y; //true
+$x===$y; //false
+
+
+
